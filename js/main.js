029e1f4a709e7f1d949719ab5befd4e2b108fd70
nodes777 <nodes777@gmail.com>
2016-01-16 15:50:30 -0500

    adds model with 5 locations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: -16.4936, lng: 145.4653},
+    zoom: 14
   });
 }
+
+var model = [
+	{
+		name: "Dougies",
+	    position: {lat: -16.495539, lng: 145.462699},
+	    map: map,
+	    title: 'Hello World!'
+	},
+	{
+		name: "Iron Bar",
+	    position: {lat: -16.481341, lng: 145.462643},
+	    map: map,
+	    title: 'Hello World!'
+	},
+	{
+		name: "Marina",
+	    position: {lat: -16.484315, lng: 145.460206},
+	    map: map,
+	    title: 'Hello World!'
+	},
+	{
+		name: "The Point",
+	    position: {lat: -16.482847, lng: 145.467855},
+	    map: map,
+	    title: 'Hello World!'
+	},
+	{
+		name: "The Beach",
+	    position: {lat: -16.493202, lng: 145.467472},
+	    map: map,
+	    title: 'Hello World!'
+	}
+
+]
